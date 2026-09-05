69b00d6877b768c4fa50256ef4112ee4f965174e
Tommy Oldfield <oldfieldtc@gmail.com>
2026-09-05 20:19:15 +0100

    Finished worker for hosting/deploying fonts

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# cf-fonts-worker
-Cloudflare worker that deploys fonts based on a URL whitelist
+# Cloudflare worker - Deploy fonts
+
+Cloudflare worker that deploys fonts based on a URL/domain whitelist.
+
+## Adding fonts
+
+Put the woff2 font file(s) in the `fonts` directory and run the following command to upload them to the KV storage locally and remotely.
+
+``` bash
+npx wrangler kv key put --namespace-id=63ec8fa695834d428455630334a579c2 "font-name.woff2" --path=./fonts/font-name.woff2
+npx wrangler kv key put --namespace-id=63ec8fa695834d428455630334a579c2 "font-name.woff2" --path=./fonts/font-name.woff2 --remote
+```
--- src/index.js
+++ src/index.js
@@ -1,20 +1,32 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
 	async fetch(request, env, ctx) {
+		if (request.method !== 'GET') {
+			return new Response('Method not allowed', { status: 405 });
+		}
+
 		const allowed = env.ALLOWED_ORIGINS.split(",").map(origin => origin.trim());
 		const origin = request.headers.get('Origin');
-
 		const isAllowed = allowed.includes(origin) || /^http:\/\/localhost(:\d{1,5})?$/.test(origin);
 
-		return new Response(`Hello ${origin}, ${isAllowed}!`);
+		if (!isAllowed) {
+			return new Response(`Forbidden`, { status: 403 })
+		}
+
+		const url = new URL(request.url);
+		const path = url.pathname.slice(1);
+		const font = await env.FONT_KV.get(path, 'arrayBuffer');
+
+		if (!font) {
+			return new Response("Not found", { status: 404 });
+		}
+
+		return new Response(font, {
+			headers: {
+				"Content-Type": "font/woff2",
+				"Access-Control-Allow-Origin": origin,
+				"Cache-Control": "public, max-age=31536000, immutable",
+				'Access-Control-Allow-Methods': 'GET'
+			},
+		});
 	},
 };
